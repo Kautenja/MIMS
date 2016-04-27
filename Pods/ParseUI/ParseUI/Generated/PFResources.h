53da9bd7f9bd9e212cc1360402abc76803eb5995
Patrick Bush <patrickbush@me.com>
2016-04-27 14:50:57 -0500

    Pod fixes

--- Pods/ParseUI/ParseUI/Generated/PFResources.h
+++ Pods/ParseUI/ParseUI/Generated/PFResources.h
@@ -1,13 +1,13 @@
 // This is an auto-generated file.
 #import <Foundation/Foundation.h>
 @interface PFResources : NSObject
-+ (NSData *)facebook_icon_png;//modified:2016-04-27 14:18:23 -0500
-+ (NSData *)facebook_icon2x_png;//modified:2016-04-27 14:18:23 -0500
-+ (NSData *)facebook_icon3x_png;//modified:2016-04-27 14:18:23 -0500
-+ (NSData *)parse_logo_png;//modified:2016-04-27 14:18:23 -0500
-+ (NSData *)parse_logo2x_png;//modified:2016-04-27 14:18:23 -0500
-+ (NSData *)parse_logo3x_png;//modified:2016-04-27 14:18:23 -0500
-+ (NSData *)twitter_icon_png;//modified:2016-04-27 14:18:23 -0500
-+ (NSData *)twitter_icon2x_png;//modified:2016-04-27 14:18:23 -0500
-+ (NSData *)twitter_icon3x_png;//modified:2016-04-27 14:18:23 -0500
++ (NSData *)facebook_icon_png;//modified:2016-04-27 14:46:52 -0500
++ (NSData *)facebook_icon2x_png;//modified:2016-04-27 14:46:52 -0500
++ (NSData *)facebook_icon3x_png;//modified:2016-04-27 14:46:52 -0500
++ (NSData *)parse_logo_png;//modified:2016-04-27 14:46:52 -0500
++ (NSData *)parse_logo2x_png;//modified:2016-04-27 14:46:52 -0500
++ (NSData *)parse_logo3x_png;//modified:2016-04-27 14:46:52 -0500
++ (NSData *)twitter_icon_png;//modified:2016-04-27 14:46:52 -0500
++ (NSData *)twitter_icon2x_png;//modified:2016-04-27 14:46:52 -0500
++ (NSData *)twitter_icon3x_png;//modified:2016-04-27 14:46:52 -0500
 @end
